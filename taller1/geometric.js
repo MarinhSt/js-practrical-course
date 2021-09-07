1dc30f4f7b398696b1ba8fe7486cfc45c0b49243
MarinhSt <st_marinh@protonmail.com>
2021-09-07 14:10:51 -0500

    Added features for work with triangles and something the arithmetic

--- taller1/geometric.js
+++ taller1/geometric.js
@@ -9,6 +9,17 @@ unidadMedida.addEventListener("change", () => {
   return console.log(unidad);
 });
 
+/* obteniendo la unidad de medida del triangulo
+const unidadMedida = document.getElementById("longitudTriangulo");
+let unidad = unidadMedida.value;
+console.log(unidad);
+
+evento que modifica el valor de unidad segun la seleccion hecha
+unidadMedida.addEventListener("change", () => {
+  unidad = document.getElementById("longitudTriangulo").value;
+  return console.log(unidad);
+}); */
+
 /* Perimetro = P | Area = A */
 /*
   Cuadrado
@@ -43,3 +54,41 @@ getPerimetroCuadrado.addEventListener("click", () => {
   const sideA = document.getElementById("ladoCuadrado").value;
   perimetroCuadrado(sideA);
 });
+
+/*
+  Triangulo
+  A = (b*h) / 2
+  P = a+b+c
+*/
+const areaTriangulo = (b, h) => {
+  const area = (b * h) / 2;
+  console.log(`El area del triangulo es de ${area} ${unidad}^2`);
+};
+const perimetroTriangulo = (a, b, c) => {
+  const area = a + b + c;
+  console.log(`El perimetro del triangulo es de ${area} ${unidad}`);
+};
+getAreaTriangulo.addEventListener("click", () => {
+  const sideB = document.getElementById("ladoTrianguloB").value;
+  const height = document.getElementById("ladoTrianguloB").value;
+  areaTriangulo(sideB, height);
+});
+getPerimetroTriangulo.addEventListener("click", () => {
+  const sideA = document.getElementById("ladoTrianguloA").value;
+  const sideB = document.getElementById("ladoTrianguloB").value;
+  const sideC = document.getElementById("ladoTrianguloC").value;
+  perimetroTriangulo(sideA, sideB, sideC);
+});
+
+/*
+  altura isosceles
+  h = √((a^2)-(b^2/4))
+*/
+const alturaIsosceles = (a, b, c) => {
+  if (a === c && c !== b && b > 0) {
+    const h = Math.sqrt([a ** 2] - [b ** 2 / 4]).toFixed(1);
+    console.log(h);
+  } else {
+    console.log("estas no son las medidas de un triangulo isosceles");
+  }
+};
